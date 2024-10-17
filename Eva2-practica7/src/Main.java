ecca0800c1bc9d51de9cb95f35f6f78f5ba4d9f1
Daniel Reza <92356684+DanielGuerrero2@users.noreply.github.com>
2024-10-17 15:03:15 -0600

    Add files via upload

---
+++ Eva2-practica7/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int val=1;
+        //while
+        System.out.println("WHILE");
+        while(val <=10){//mientras que los parentesis sean correctos se repitera la instruccion
+            System.out.print(val+"-");
+            val++;
+
+        }
+
+        //do while
+        System.out.println("");
+        System.out.println("do while");
+        val=1;
+        do{
+            System.out.print(val+"-");
+            val++;
+        }while(val <=10);//repite las instrucciones cuando el parente4cis es verdad
+
+    }
+
+}
