1e36bb46d346e61cc3a11e41c1e7ca3a56b054a6
Daniel Reza <92356684+DanielGuerrero2@users.noreply.github.com>
2024-10-22 17:23:46 -0600

    Add files via upload

---
+++ Eva2-practica17/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] names = {"Daniel" , "evelyn", "Sam", "Carlos", "Luis"};
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
